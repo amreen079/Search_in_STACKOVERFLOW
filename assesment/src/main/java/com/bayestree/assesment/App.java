8fcd25641d85f13cd935221fda49990406fbf501
amreen079 <69970728+amreen079@users.noreply.github.com>
2020-08-30 16:29:29 +0530

    updated app.java

--- assesment/src/main/java/com/bayestree/assesment/App.java
+++ assesment/src/main/java/com/bayestree/assesment/App.java
@@ -1,9 +1,13 @@
 package com.bayestree.assesment;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.bayestree.assesment.service.Stackservice;
 
+@SpringBootApplication
 public class App {
 	public static void main(String[] args) {
+		SpringApplication.run(App.class);
 		Stackservice service = new Stackservice();
 		service.res();
 	}
